63116b43551b250b7f37c97827c37841e2bb6a78
danielrab.work <s17b1_rabinovich@179.ru>
2024-07-12 23:56:25 +0300

    finished the first functional version of /submit

--- src/commands/info/submit.py
+++ src/commands/info/submit.py
@@ -1,13 +1,21 @@
 from discord.ext import commands
 import discord
+from sql import get_db
+from discord.commands.context import ApplicationContext
+from utils.constants import SUBMISSIONS_CHANNEL
 
 
 class Submit(commands.Cog):
+    CATEGORIES = [
+        '60s: Hay', '60s: Wood', "60s: Carrots", "60s: Pumpkins", "60s: Power", "60s: Gold", "60s: Cactus", "60s: Bones",
+        "Maze: 300", "Maze: 100", "Maze: 200 8x8", "Maze: 10x20"
+    ]
+
     def __init__(self, bot):
         self.bot = bot
 
     @commands.slash_command(name='submit', description="Submit a score")
-    async def submit(self, ctx, category: discord.Option(str, choices=['60s: Hay', '60s: Wood']), score: float, proof: discord.Attachment):
+    async def submit(self, ctx: ApplicationContext, category: discord.Option(str, choices=CATEGORIES), score: float, proof: discord.Attachment):
         """A submit command that submits a score.
 
         Args:
@@ -16,7 +24,8 @@ class Submit(commands.Cog):
             score (int): The score being claimed.
             proof (discord.Attachment): A file containing the proof of the score.
         """
-        await ctx.respond(f"Submitting {category}!", ephemeral=True)
+        await ctx.bot.get_channel(SUBMISSIONS_CHANNEL).send(f"`{ctx.user.name}` submitted a score of `{score}` in the `{category}` category. {proof.url}")
+        await ctx.respond(f"Submitting a score of `{score}` in `{category}`. The score will be added to the leaderboard after review. Please be patient!", ephemeral=True)
 
 
 def setup(bot):
